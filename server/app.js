aaa2d951ed2c09c912bf7ef778a4cae8f240f6f6
Zainab Anwaar <pisces.zaiby03@gmail.com>
2023-06-22 18:39:33 +0500

    Add Get and Post Route in app.js

---
+++ server/app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const cors = require("cors")
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors())
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/EmployeeDB", { useNewUrlParser: true });
+
+const employeeSchema = {
+    _id: Object,
+    firstName: String,
+    secondName: String,
+    email: String,
+    phone: String,
+
+};
+
+const Employee = mongoose.model("employees", employeeSchema);
+
+
+app.route("/employees")
+    .get(async function (req, res) {
+        try {
+            const foundEmp = await Employee.find();
+            res.send(foundEmp);
+        } catch (err) {
+            res.send(err);
+        }
+    })
+
+app.route("/submit").post(async function (req, res) {
+    const newEmp = new Employee({
+        _id: new mongoose.Types.ObjectId(),
+        firstName: req.body.firstName,
+        secondName: req.body.secondName,
+        email: req.body.email,
+        phone: req.body.phone,
+    });
+    try {
+        const savedEmp = await newEmp.save();
+        res.send(savedEmp);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.listen(4000, function () {
+    console.log("Server started on port 4000");
+});
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,49 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 function Form() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleSubmit = (event) => {
+
+        // Create an object with the form data
+        const formData = {
+            firstName,
+            lastName,
+            email,
+            phone
+        };
+
+        // Make the POST request
+        fetch('http://localhost:4000/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response data
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle the error
+                console.error(error);
+            });
+    };
+
+    const handleReset = () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPhone('');
+    };
+
     return (
-        <div>
-
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="firstName" class="col-form-label">First Name</label>
+        <form onSubmit={handleSubmit}>
+            <div className="row g-3 align-items-center">
+                <div className="col-auto">
+                    <label htmlFor="firstName" className="col-form-label">First Name</label>
                 </div>
-                <div class="col-auto">
-                    <input type="text" id="firstName" class="form-control" aria-describedby="firstName" />
+                <div className="col-auto">
+                    <input
+                        type="text"
+                        required
+                        id="firstName"
+                        className="form-control"
+                        aria-describedby="firstName"
+                        value={firstName}
+                        onChange={event => setFirstName(event.target.value)}
+                    />
                 </div>
-
             </div>
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="lastName" class="col-form-label">Last Name</label>
-                </div>
-                <div class="col-auto">
-                    <input type="text" id="lastName" class="form-control" aria-describedby="lastName" />
-                </div>
 
+            <div className="row g-3 align-items-center">
+                <div className="col-auto">
+                    <label htmlFor="lastName" className="col-form-label">Last Name</label>
+                </div>
+                <div className="col-auto">
+                    <input
+                        type="text"
+                        required
+                        id="lastName"
+                        className="form-control"
+                        aria-describedby="lastName"
+                        value={lastName}
+                        onChange={event => setLastName(event.target.value)}
+                    />
+                </div>
             </div>
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="email" class="col-form-label">Email</label>
-                </div>
-                <div class="col-auto">
-                    <input type="email" id="email" class="form-control" aria-describedby="email" />
-                </div>
 
+            <div className="row g-3 align-items-center">
+                <div className="col-auto">
+                    <label htmlFor="email" className="col-form-label">Email</label>
+                </div>
+                <div className="col-auto">
+                    <input
+                        type="email"
+                        required
+                        id="email"
+                        className="form-control"
+                        aria-describedby="email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                </div>
             </div>
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="phone" class="col-form-label">Phone</label>
-                </div>
-                <div class="col-auto">
-                    <input type="number" id="phone" class="form-control" aria-describedby="phone" />
-                </div>
 
+            <div className="row g-3 align-items-center">
+                <div className="col-auto">
+                    <label htmlFor="phone" className="col-form-label">Phone</label>
+                </div>
+                <div className="col-auto">
+                    <input
+                        type="tel"
+                        required
+                        id="phone"
+                        className="form-control"
+                        aria-describedby="phone"
+                        value={phone}
+                        onChange={event => setPhone(event.target.value)}
+                    />
+                </div>
             </div>
-        </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="reset" className="btn btn-primary" onClick={handleReset}>Clear</button>
+        </form>
     );
-
 }
-
-
 
 export default Form;
